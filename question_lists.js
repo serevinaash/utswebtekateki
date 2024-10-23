42de0783d82b6058c55bba492d08e3f84e8f0241
serevina sherly maulida <101394654+serevinaash@users.noreply.github.com>
2024-10-23 18:56:17 +0700

    Initial commit

---
+++ question_lists.js
@@ -0,0 +1,12 @@
+var quiz = [
+    [0, 0, "Ibu Kota Indonesia", "JAKARTA", "datar"],
+    [1, 6, "Planet terbesar di tata surya", "JUPITER", "datar"],
+    [2, 0, "Satuan waktu yang lebih besar dari detik", "MENIT", "turun"],
+    [3, 2, "Gas yang kita hirup untuk bernapas", "OKSIGEN", "datar"],
+    [5, 5, "Nama benua yang terbesar", "ASIA", "turun"],
+    [0, 9, "Nama hewan berkaki empat, besar, dan berbulu lebat", "BERUANG", "turun"],
+    [7, 3, "Benua terkecil di dunia", "AUSTRALIA", "datar"],
+    [8, 0, "Alat yang digunakan untuk menulis di papan tulis", "KAPUR", "turun"],
+    [9, 4, "Negara dengan Menara Eiffel", "PRANCIS", "datar"],
+    [2, 12, "Sebuah negara di Asia Selatan", "INDIA", "turun"]
+];
